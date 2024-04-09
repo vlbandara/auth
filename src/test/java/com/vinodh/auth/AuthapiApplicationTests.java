7bbb943b71262361f69a466d0d1b8045bb1a2854
vlbandara <137267283+vlbandara@users.noreply.github.com>
2024-04-09 17:14:32 +0530

    Initial commit

---
+++ src/test/java/com/vinodh/auth/AuthapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinodh.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
